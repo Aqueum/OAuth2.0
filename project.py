4f2bd16baaff146557b670e98732690a32d195fe
Aqueum <martin@aqueum.com>
2017-07-11 15:21:05 +0100

    Add creator name and image to their editable menu

--- project.py
+++ project.py
@@ -277,7 +277,7 @@ def showMenu(restaurant_id):
         restaurant_id=restaurant_id).all()
     creator = getUserInfo(restaurant.user_id)
     if login_session['user_id'] == restaurant.user_id:
-        return render_template('menu.html', items=items, restaurant=restaurant)
+        return render_template('menu.html', items=items, restaurant=restaurant, creator=creator)
     else:
         return render_template('publicmenu.html', items=items, restaurant=restaurant, creator=creator)
 
